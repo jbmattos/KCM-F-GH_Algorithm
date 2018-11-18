distance calculation: bugs fixed

cluster.py
@@ -8,19 +8,26 @@ class Cluster:
         self.prototype = []
         self.size = None
         self.kernel_matrix = None
-        self.kernel = 1
+        self.kernel = None
 
         self.cluster_init(no_of_examples)
 
     def cluster_init(self, n):
         self.prototype.append(random.randint(0, n-1))
         self.size = len(self.prototype)
+        self.kernel_matrix = np.full(shape=(1, 1), fill_value=1)
+        self.kernel = 1
 
         return
 
     def set_cluster_kernel(self, kernel_matrix):
 
         n = self.size
+
+        # if n == 1:
+        #     self.kernel_matrix = np.full(shape=(n, n), fill_value=1)
+        #     self.kernel = 1
+
         cluster_kernel_matrix = np.zeros(shape=(n, n))
 
         for e1_idx, element_1 in enumerate(self.prototype):

functions.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from cluster import Cluster
 from sklearn.metrics.pairwise import euclidean_distances
@@ -80,11 +81,11 @@ def generate_distances(kernel_matrix, clusters):                # input: (np.mat
 
     for example in range(n):                            # iterating all examples
 
-        kernel_sum = 0
         for c_idx, cluster in enumerate(clusters):      # iterating all clusters (for each example)
+            kernel_sum = 0
             for element in cluster.prototype:
                 kernel_sum += kernel_matrix[example, element]
-            distances_matrix[example, c_idx] = 1 - (2 * kernel_sum)/cluster.size + cluster.kernel/(cluster.size ** 2)
+            distances_matrix[example, c_idx] = 1 - (2 * (kernel_sum/cluster.size)) + cluster.kernel/(cluster.size ** 2)
 
     return distances_matrix
 
@@ -92,6 +93,7 @@ def generate_distances(kernel_matrix, clusters):                # input: (np.mat
 def generate_partition(kernel_matrix, distances_matrix, clusters):  # input: (np.matrix, np.matrix, list_of_objects)
 
     elements_cluster_location = distances_matrix.argmin(axis=1)
+    n = len(elements_cluster_location)
 
     for c_idx, cluster in enumerate(clusters):
 
@@ -100,6 +102,13 @@ def generate_partition(kernel_matrix, distances_matrix, clusters):  # input: (np
 
         for e in elements:
             cluster_elements.append(e[0])
+
+        if not cluster_elements:
+            cluster_new_ex = random.randint(0, n - 1)
+            old_cluster_ex = elements_cluster_location[cluster_new_ex]
+            elements_cluster_location[cluster_new_ex] = c_idx
+            if old_cluster_ex < c_idx:
+                clusters[old_cluster_ex].prototype.remove(cluster_new_ex)
 
         cluster.prototype = cluster_elements[:]
         cluster.size = len(cluster_elements)

main.py
@@ -21,13 +21,11 @@ def main():
 
 
     # INITIALIZATION
-    clusters = cluster_initialization(c, n)             # vector of cluster objects
+    clusters = cluster_initialization(c, n)  # vector of cluster objects
     hp_vector = np.full(p, fill_value=gama**(1/p))      # hyper-parameter vector
 
     kernel_matrix = generate_kernel_matrix(view.values, hp_vector)
     distances_matrix = generate_distances(kernel_matrix, clusters)
-    print(np.array_equal(distances_matrix[:,0], distances_matrix[:,1]))
-
 
     clusters, examples_location = generate_partition(kernel_matrix, distances_matrix, clusters)
     objective_function = get_objective_fnc(clusters, distances_matrix)
@@ -47,6 +45,7 @@ def main():
         objective_function = get_objective_fnc(clusters, distances_matrix)
         print('Objective function = ', objective_function)
 
+        print('Examples location equal comparison: ', np.array_equal(examples_location, examples_new_location))
         if np.array_equal(examples_location, examples_new_location):
             test = 0
 